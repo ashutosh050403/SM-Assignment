7b835c747bdea97065cbdea6654cb79ec9e0c8d6
ashutosh050403 <ashu.maur678678@gmail.com>
2025-12-12 16:22:51 +0530

    created config

---
+++ src/main/java/com/ashutosh/smassignment/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.ashutosh.smassignment.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+
+                registry.addMapping("/**")                // Allow all endpoints
+                        .allowedOrigins("*")             // Allow all origins (Postman/web)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
