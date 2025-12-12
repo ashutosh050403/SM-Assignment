846fc4f5afbeec53d13a7f0013bb44d98a7428f0
ashutosh050403 <ashu.maur678678@gmail.com>
2025-12-12 16:11:20 +0530

    created entities, dtos and utils

---
+++ src/test/java/com/ashutosh/smassignment/SmAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashutosh.smassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
